4aa4a4ee99f115c3f98ac0032f723444655bb9de
donrestarone <shashikejayatunge@gmail.com>
2018-05-03 14:53:23 -0400

    finish assignment

--- simon_says.rb
+++ simon_says.rb
@@ -5,3 +5,21 @@ end
 def shout(word)
   return "#{word.upcase}"
 end
+
+def repeat(word, repetition)
+  spaced_word = " #{word}"
+  output = ""
+  repetition.times {output += spaced_word}
+  return output.strip
+end
+
+def start_of_word(word, index)
+  array = word.split("")
+  convert = index - 1
+  string = array[0..convert].join
+end
+
+def first_word(string)
+  first_word = string.split.first
+
+end
